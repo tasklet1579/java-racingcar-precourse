d03a617a706af6df4c3e0a9cad1f2bea6624451f
tasklet1579 <tasklet1579@gmail.com>
2022-04-22 16:15:05 +0900

    :sparkles: feat(RacingGame, RacingGameResult) : 자동차 게임을 구현한다 - 자동차 게임을 시작한다 - 자동차 이름을 입력받는다 - 자동차 진행 횟수를 입력받는다 - 자동차 이름에 문제가 없는지 확인한다 - 자동차 진행 횟수에 문제가 없는지 확인한다 - 자동차 게임 실행 결과 헤더를 출력한다 - 자동차별로 랜덤한 숫자를 부여한다 - 자동차의 이름과 이동거리를 출력한다 - 자동차 게임의 우승자를 출력한다 - 현재 턴에서의 게임 승리자를 찾는다 - 리스트 형태의 String을 콤마와 조인해서 반환한다

---
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -0,0 +1,125 @@
+package racingcar.controller;
+
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingCarPart;
+import racingcar.domain.RacingGameConsole;
+import racingcar.domain.RandomIntegerGenerator;
+import racingcar.dto.RacingGameResult;
+import racingcar.view.RacingGameOutputView;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RacingGame {
+    private List<RacingCar> cars = new LinkedList<>();
+    private RacingCarPart part = new RacingCarPart();
+    private int count = 0;
+
+    public RacingGame() { }
+
+    public void playRacing() {
+        while (isCarNamesOk()) {
+            this.part = readCarNames();
+        }
+
+        while (isMovingCountOk()) {
+            this.count = readMovingCount();
+        }
+
+        for (String name : this.part.getNames()) {
+            RacingCar car = new RacingCar(name);
+            cars.add(car);
+        }
+
+        printRacingGameHeader();
+
+        while (this.count > 0) {
+            startRacingCars(this.cars);
+            printRacingGamers(this.cars);
+            this.count--;
+        }
+
+        printRacingGameWinners(this.cars);
+    }
+
+    public boolean isCarNamesOk() {
+        return this.part.size() == 0;
+    }
+
+    public RacingCarPart readCarNames() {
+        try {
+            RacingGameOutputView view = new RacingGameOutputView();
+            view.printConsoleMessage("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            RacingGameConsole console = new RacingGameConsole();
+            return console.readCarNames();
+        } catch (Exception ignored) {
+        }
+        return new RacingCarPart();
+    }
+
+    public boolean isMovingCountOk() {
+        return this.count <= 0;
+    }
+
+    public int readMovingCount() {
+        try {
+            RacingGameOutputView view = new RacingGameOutputView();
+            view.printConsoleMessage("시도할 회수는 몇회인가요?");
+            RacingGameConsole console = new RacingGameConsole();
+            return console.readMovingCount();
+        } catch (Exception ignored) {
+        }
+        return 0;
+    }
+
+    public void startRacingCars(List<RacingCar> cars) {
+        RandomIntegerGenerator generator = new RandomIntegerGenerator(0, 9);
+        for (RacingCar car : cars) {
+            car.moveForward(generator.pickRandomIntegerInRange());
+        }
+    }
+
+    public void printRacingGameHeader() {
+        RacingGameOutputView view = new RacingGameOutputView();
+        view.printConsoleMessage("실행 결과");
+    }
+
+    public void printRacingGamers(List<RacingCar> cars) {
+        RacingGameOutputView view = new RacingGameOutputView();
+        for (RacingCar car : cars) {
+            view.printConsoleMessage(car.information());
+        }
+        System.out.println();
+    }
+
+    public void printRacingGameWinners(List<RacingCar> cars) {
+        List<RacingCar> winners = new LinkedList<>();
+        RacingGameResult result = new RacingGameResult(winners, 0);
+        for (RacingCar car : cars) {
+            result = measureCarDistanceFromStartLine(car, result);
+        }
+
+        RacingGameOutputView view = new RacingGameOutputView();
+        view.printConsoleMessage("최종 우승자: " + joinNames(result.winners()));
+    }
+
+    public RacingGameResult measureCarDistanceFromStartLine(RacingCar car, RacingGameResult result) {
+        if (car.distance() == result.maxDistance()) {
+            result.add(car);
+        }
+        if (car.distance() > result.maxDistance()) {
+            result.changeMaxDistance(car.distance());
+            result.clear();
+            result.add(car);
+        }
+        return result;
+    }
+
+    public String joinNames(List<RacingCar> winners) {
+        List<String> names = new LinkedList<>();
+        for (RacingCar car : winners) {
+            names.add(car.name());
+        }
+        return String.join(", ", names);
+    }
+}
